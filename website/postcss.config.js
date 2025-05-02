37647029a9dbe6a4f66ab74143c8b9706cd6e3f7
Sachin P. Bulchandani <3011sachin@gmail.com>
2025-05-02 05:41:50 -0400

    Initila commit for webiste, completed hero, skills, and background section....

---
+++ website/postcss.config.js
@@ -0,0 +1,7 @@
+// website/postcss.config.js
+module.exports = {
+    plugins: {
+      tailwindcss: {}, // Tells PostCSS to process Tailwind directives
+      autoprefixer: {}, // Adds vendor prefixes for browser compatibility
+    },
+  }
