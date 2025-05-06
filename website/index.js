a98fdd9d80ec78cfd0b72ade70c899a1abe51bf6
Sachin P. Bulchandani <3011sachin@gmail.com>
2025-05-06 13:52:30 -0400

    Basic Portfolio structure is ready....!!

--- website/index.js
+++ website/index.js
@@ -1,296 +1,244 @@
 // initialization
+const RESPONSIVE_WIDTH = 1024;
 
-const RESPONSIVE_WIDTH = 1024
+let headerWhiteBg = false;
+let isHeaderCollapsed = window.innerWidth < RESPONSIVE_WIDTH;
+const collapseBtn = document.getElementById("collapse-btn");
+const collapseHeaderItems = document.getElementById("collapsed-header-items");
 
-let headerWhiteBg = false
-let isHeaderCollapsed = window.innerWidth < RESPONSIVE_WIDTH
-const collapseBtn = document.getElementById("collapse-btn")
-const collapseHeaderItems = document.getElementById("collapsed-header-items")
-
-const navToggle = document.querySelector("#nav-dropdown-toggle-0")
-const navDropdown = document.querySelector("#nav-dropdown-list-0")
-
+const navToggle = document.querySelector("#nav-dropdown-toggle-0");
+const navDropdown = document.querySelector("#nav-dropdown-list-0");
 
 function onHeaderClickOutside(e) {
-
     if (!collapseHeaderItems.contains(e.target)) {
-        toggleHeader()
+        toggleHeader();
     }
-
 }
-
 
 function toggleHeader() {
     if (isHeaderCollapsed) {
-        // collapseHeaderItems.classList.remove("max-md:tw-opacity-0")
-        collapseHeaderItems.classList.add("max-lg:!tw-opacity-100", "tw-min-h-[90vh]")
-        collapseHeaderItems.style.height = "90vh"
-        collapseBtn.classList.remove("bi-list")
-        collapseBtn.classList.add("bi-x", "max-lg:tw-fixed")
-        isHeaderCollapsed = false
-
-        document.body.classList.add("modal-open")
-
-        setTimeout(() => window.addEventListener("click", onHeaderClickOutside), 1)
-
+        collapseHeaderItems.classList.add("max-lg:!tw-opacity-100", "tw-min-h-[90vh]");
+        collapseHeaderItems.style.height = "90vh";
+        collapseBtn.classList.remove("bi-list");
+        collapseBtn.classList.add("bi-x", "max-lg:tw-fixed");
+        isHeaderCollapsed = false;
+        document.body.classList.add("modal-open");
+        setTimeout(() => window.addEventListener("click", onHeaderClickOutside), 1);
     } else {
-        collapseHeaderItems.classList.remove("max-lg:!tw-opacity-100", "tw-min-h-[90vh]")
-        collapseHeaderItems.style.height = "0vh"
-        
-        collapseBtn.classList.remove("bi-x", "max-lg:tw-fixed")  
-        
-        collapseBtn.classList.add("bi-list")
-        document.body.classList.remove("modal-open")
-
-        isHeaderCollapsed = true
-        window.removeEventListener("click", onHeaderClickOutside)
-
+        collapseHeaderItems.classList.remove("max-lg:!tw-opacity-100", "tw-min-h-[90vh]");
+        collapseHeaderItems.style.height = "0vh";
+        collapseBtn.classList.remove("bi-x", "max-lg:tw-fixed");
+        collapseBtn.classList.add("bi-list");
+        document.body.classList.remove("modal-open");
+        isHeaderCollapsed = true;
+        window.removeEventListener("click", onHeaderClickOutside);
     }
 }
 
 function responsive() {
-    if (!isHeaderCollapsed){
-        toggleHeader()
+    if (!isHeaderCollapsed) {
+        toggleHeader();
     }
-
     if (window.innerWidth > RESPONSIVE_WIDTH) {
-        collapseHeaderItems.style.height = ""
-        navToggle.addEventListener("mouseenter", openNavDropdown)
-        navToggle.addEventListener("mouseleave", navMouseLeave)
-
+        collapseHeaderItems.style.height = "";
+        navToggle.addEventListener("mouseenter", openNavDropdown);
+        navToggle.addEventListener("mouseleave", navMouseLeave);
     } else {
-        isHeaderCollapsed = true
-        navToggle.removeEventListener("mouseenter", openNavDropdown)
-        navToggle.removeEventListener("mouseleave", navMouseLeave)
+        isHeaderCollapsed = true;
+        navToggle.removeEventListener("mouseenter", openNavDropdown);
+        navToggle.removeEventListener("mouseleave", navMouseLeave);
     }
 }
-responsive()
-window.addEventListener("resize", responsive)
+responsive();
+window.addEventListener("resize", responsive);
 
 /** Dark and light theme */
 if (localStorage.getItem('color-mode') === 'dark' || (!('color-mode' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('tw-dark')
-    updateToggleModeBtn()
+    document.documentElement.classList.add('tw-dark');
+    updateToggleModeBtn();
 } else {
-    document.documentElement.classList.remove('tw-dark')
-    updateToggleModeBtn()
+    document.documentElement.classList.remove('tw-dark');
+    updateToggleModeBtn();
 }
 
-function toggleMode(){
-    //toggle between dark and light mode
-    document.documentElement.classList.toggle("tw-dark")
-    updateToggleModeBtn()
-    
+function toggleMode() {
+    document.documentElement.classList.toggle("tw-dark");
+    updateToggleModeBtn();
 }
 
-function updateToggleModeBtn(){
-
-    const toggleIcon = document.querySelector("#toggle-mode-icon")
-    
-    if (document.documentElement.classList.contains("tw-dark")){
-        // dark mode
-        toggleIcon.classList.remove("bi-sun")
-        toggleIcon.classList.add("bi-moon")
-        localStorage.setItem("color-mode", "dark")
-        
-    }else{
-        toggleIcon.classList.add("bi-sun")
-        toggleIcon.classList.remove("bi-moon")
-        localStorage.setItem("color-mode", "light")
-    }
-
-}
-
-
-const promptWindow =  new Prompt("#pixa-playground")
-const promptForm = document.querySelector("#prompt-form")
-const promptInput = promptForm.querySelector("input[name='prompt']")
-
-const MAX_PROMPTS = 3
-
-promptForm.addEventListener("submit", (event) => {
-    event.preventDefault()
-
-    // window.open("https://github.com/PaulleDemon", "_blank")
-
-    if (promptWindow.promptList.length >= MAX_PROMPTS)
-        return false
-
-    promptWindow.addPrompt(promptInput.value)
-    promptInput.value = ""
-    
-    if (promptWindow.promptList.length >= MAX_PROMPTS){
-        // prompt signup once the user makes 3 prompts, ideally must be throttled via backend API
-        const signUpPrompt = document.querySelector("#signup-prompt")
-        signUpPrompt.classList.add("tw-scale-100")
-        signUpPrompt.classList.remove("tw-scale-0")
-
-        promptForm.querySelectorAll("input").forEach(e => {e.disabled = true})
-    }
-
-    return false
-})
-
-const dropdowns = document.querySelectorAll('.dropdown')
-dropdowns.forEach(dropdown => new Dropdown(`#${dropdown.id}`, promptWindow.setAIModel))
-
-
-navToggle.addEventListener("click", toggleNavDropdown)
-navDropdown.addEventListener("mouseleave", closeNavDropdown)
-
-function toggleNavDropdown(){
-
-    if (navDropdown.getAttribute("data-open") === "true"){
-        closeNavDropdown()
-    }else{
-        openNavDropdown()
+function updateToggleModeBtn() {
+    const toggleIcon = document.querySelector("#toggle-mode-icon");
+    if (document.documentElement.classList.contains("tw-dark")) {
+        toggleIcon.classList.remove("bi-sun");
+        toggleIcon.classList.add("bi-moon");
+        localStorage.setItem("color-mode", "dark");
+    } else {
+        toggleIcon.classList.add("bi-sun");
+        toggleIcon.classList.remove("bi-moon");
+        localStorage.setItem("color-mode", "light");
     }
 }
 
-function navMouseLeave(){
-    setTimeout(closeNavDropdown, 100)
+const promptWindow = new Prompt("#pixa-playground");
+const promptForm = document.querySelector("#prompt-form");
+const promptInput = promptForm?.querySelector("input[name='prompt']");
+
+const MAX_PROMPTS = 3;
+
+if (promptForm) {
+    promptForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (promptWindow.promptList.length >= MAX_PROMPTS) return false;
+        promptWindow.addPrompt(promptInput.value);
+        promptInput.value = "";
+        if (promptWindow.promptList.length >= MAX_PROMPTS) {
+            const signUpPrompt = document.querySelector("#signup-prompt");
+            signUpPrompt.classList.add("tw-scale-100");
+            signUpPrompt.classList.remove("tw-scale-0");
+            promptForm.querySelectorAll("input").forEach(e => { e.disabled = true });
+        }
+        return false;
+    });
 }
 
-function openNavDropdown(event){
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => new Dropdown(`#${dropdown.id}`, promptWindow.setAIModel));
 
-    navDropdown.classList.add("tw-opacity-100", "tw-scale-100", 
-                            "max-lg:tw-min-h-[450px]", "max-lg:!tw-h-fit", "tw-min-w-[320px]")
-    
-    navDropdown.setAttribute("data-open", true)
+navToggle.addEventListener("click", toggleNavDropdown);
+navDropdown.addEventListener("mouseleave", closeNavDropdown);
 
-}
-
-function closeNavDropdown(event){
-
-    // console.log("event target: ", event.target, event.target.contains(navDropdown))
-    
-    if (navDropdown.matches(":hover")){
-        return
+function toggleNavDropdown() {
+    if (navDropdown.getAttribute("data-open") === "true") {
+        closeNavDropdown();
+    } else {
+        openNavDropdown();
     }
-
-    navDropdown.classList.remove("tw-opacity-100", "tw-scale-100", 
-        "max-lg:tw-min-h-[450px]", "tw-min-w-[320px]", "max-lg:!tw-h-fit",)
-
-    navDropdown.setAttribute("data-open", false)
-
 }
 
-
-const videoBg = document.querySelector("#video-container-bg")
-const videoContainer = document.querySelector("#video-container")
-
-function openVideo(){
-    videoBg.classList.remove("tw-scale-0", "tw-opacity-0")
-    videoBg.classList.add("tw-scale-100", "tw-opacity-100")
-    videoContainer.classList.remove("tw-scale-0")
-    videoContainer.classList.add("tw-scale-100")
-
-    document.body.classList.add("modal-open")
+function navMouseLeave() {
+    setTimeout(closeNavDropdown, 100);
 }
 
-function closeVideo(){
-    videoContainer.classList.add("tw-scale-0")
-    videoContainer.classList.remove("tw-scale-100")
+function openNavDropdown() {
+    navDropdown.classList.add("tw-opacity-100", "tw-scale-100", "max-lg:tw-min-h-[450px]", "max-lg:!tw-h-fit", "tw-min-w-[320px]");
+    navDropdown.setAttribute("data-open", true);
+}
 
+function closeNavDropdown() {
+    if (navDropdown.matches(":hover")) return;
+    navDropdown.classList.remove("tw-opacity-100", "tw-scale-100", "max-lg:tw-min-h-[450px]", "tw-min-w-[320px]", "max-lg:!tw-h-fit");
+    navDropdown.setAttribute("data-open", false);
+}
+
+const videoBg = document.querySelector("#video-container-bg");
+const videoContainer = document.querySelector("#video-container");
+
+function openVideo() {
+    videoBg.classList.remove("tw-scale-0", "tw-opacity-0");
+    videoBg.classList.add("tw-scale-100", "tw-opacity-100");
+    videoContainer.classList.remove("tw-scale-0");
+    videoContainer.classList.add("tw-scale-100");
+    document.body.classList.add("modal-open");
+}
+
+function closeVideo() {
+    videoContainer.classList.add("tw-scale-0");
+    videoContainer.classList.remove("tw-scale-100");
     setTimeout(() => {
-        videoBg.classList.remove("tw-scale-100", "tw-opacity-100")
-        videoBg.classList.add("tw-scale-0", "tw-opacity-0")
-    }, 400)
-   
-
-    document.body.classList.remove("modal-open")
-
+        videoBg.classList.remove("tw-scale-100", "tw-opacity-100");
+        videoBg.classList.add("tw-scale-0", "tw-opacity-0");
+    }, 400);
+    document.body.classList.remove("modal-open");
 }
 
 /**
  * Animations
  */
-
 const typed = new Typed('#prompts-sample', {
-    strings: ["How to solve a rubik's cube? Step by step guide", 
-                "What's Pixa playground?", 
-                "How to build an AI SaaS App?", 
-                "How to integrate Pixa API?"],
+    strings: ["How to solve a rubik's cube? Step by step guide", "What's Pixa playground?", "How to build an AI SaaS App?", "How to integrate Pixa API?"],
     typeSpeed: 80,
-    smartBackspace: true, 
+    smartBackspace: true,
     loop: true,
     backDelay: 2000,
-})
+});
 
-gsap.registerPlugin(ScrollTrigger)
-
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 gsap.to(".reveal-up", {
     opacity: 0,
     y: "100%",
-})
+});
 
-
-// straightens the slanting image
 gsap.to("#dashboard", {
-
     scale: 1,
     translateY: 0,
-    // translateY: "0%",
     rotateX: "0deg",
     scrollTrigger: {
         trigger: "#hero-section",
         start: window.innerWidth > RESPONSIVE_WIDTH ? "top 95%" : "top 70%",
         end: "bottom bottom",
         scrub: 1,
-        // markers: true,
     }
+});
 
-})
-
-const faqAccordion = document.querySelectorAll('.faq-accordion')
-
-faqAccordion.forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        this.classList.toggle('active')
-
-        // Toggle 'rotate' class to rotate the arrow
-        let content = this.nextElementSibling
-        let icon = this.querySelector(".bi-plus")
-
-        // content.classList.toggle('!tw-hidden')
+const faqAccordion = document.querySelectorAll('.faq-accordion');
+faqAccordion.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        this.classList.toggle('active');
+        let content = this.nextElementSibling;
+        let icon = this.querySelector(".bi-plus");
         if (content.style.maxHeight === '240px') {
-            content.style.maxHeight = '0px'
-            content.style.padding = '0px 18px'
-            icon.style.transform = "rotate(0deg)"
-            
+            content.style.maxHeight = '0px';
+            content.style.padding = '0px 18px';
+            icon.style.transform = "rotate(0deg)";
         } else {
-            content.style.maxHeight = '240px'
-            content.style.padding = '20px 18px'
-            icon.style.transform = "rotate(45deg)"
+            content.style.maxHeight = '240px';
+            content.style.padding = '20px 18px';
+            icon.style.transform = "rotate(45deg)";
         }
-    })
-})
-
-
-
-// ------------- reveal section animations ---------------
+    });
+});
 
 const sections = gsap.utils.toArray("section");
-
 sections.forEach((sec) => {
     const revealUptimeline = gsap.timeline({
         paused: true,
         scrollTrigger: {
             trigger: sec,
-            start: "10% 80%", // top of trigger hits the top of viewport
+            start: "10% 80%",
             end: "20% 90%",
-            // markers: true, // Uncomment for debugging if needed
-            // scrub: 1, // Uncomment for smooth scrubbing if desired
         }
     });
-
-    // Exclude absolutely positioned elements (e.g., "Discover More" div) from reveal-up animation
     const revealUpElements = sec.querySelectorAll(".reveal-up:not(.tw-absolute .reveal-up)");
     revealUptimeline.to(revealUpElements, {
         opacity: 1,
         duration: 0.8,
         y: "0%",
         stagger: 0.2,
+    });
+});
+
+// Fix anchor links to account for fixed header
+document.addEventListener('DOMContentLoaded', function() {
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+    anchorLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+                const offsetPosition = targetPosition - headerHeight - 10;
+
+                gsap.to(window, {
+                    duration: 1,
+                    scrollTo: offsetPosition,
+                    ease: 'power2.out'
+                });
+            }
+        });
     });
 });
